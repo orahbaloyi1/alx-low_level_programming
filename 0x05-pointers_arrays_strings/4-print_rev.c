69f330fecb806dc88d83d05b483de535ce70174c
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-02-28 13:14:14 -0800

    Reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: The string to print
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c = c - 1; c >= 0; c--)
+		_putchar(s[c]);
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
