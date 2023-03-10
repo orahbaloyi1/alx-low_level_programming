a010082a1129bde886dfa7b28268434a840ccdf4
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-03-10 07:38:15 -0800

    Function that returns the factorial of a given number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x to the power of y
+ * @x: value
+ * @y: power
+ * Return: Always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
