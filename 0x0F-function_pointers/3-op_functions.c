d29da8fbd2556436f7a8d9f11d56ff17600937e0
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-03-23 13:10:41 -0700

    Function Pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,10 @@
 #include "3-calc.h"
 
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 
 /**
 * op_add - Returns the sum of two numbers.
@@ -15,6 +13,7 @@ int op_mod(int a, int b);
 *
 * Return: The sum of a and b.
 */
+
 int op_add(int a, int b)
 {
 	return (a + b);
