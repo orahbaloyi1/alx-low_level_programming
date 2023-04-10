40fd23808e3fa83407ef3a2156bca2c44e5e64a2
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-04-10 11:52:11 -0700

    Cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,91 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer
+ * Return: A pointer to the new  buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf  = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		fprintf(stderr, "Error: Failed to creat buffer for file %s\n", file);
+		exit(EXIT_FAILURE);
+	}
+	return (buf);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents to another file.
+ * @argc: The number of arguments.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ * Description: If exit code 97.
+ *              If exit code 98.
+ *              If exit code 99.
+ *              If exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from;
+	int to;
+	int r;
+	int w;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buf = create_buffer(argv[2]);
+
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buf, 1024);
+	if (from == -1 || r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buf);
+		exit(98);
+	}
+
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	w = write(to, buf, r);
+	if (to == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buf);
+		exit(99);
+
+	} while (r > 0);
+
+	free(buf);
+	close_file(from);
+	close_file(to);
+	return (0);
+}
